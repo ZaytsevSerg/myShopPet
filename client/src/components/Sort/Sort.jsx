ad0a44eb00e93e432f8fa81fc8d15aeb844e0637
Serg <zaytsergo@gmail.com>
2022-11-08 14:57:08 +0300

    sortDone

--- client/src/components/Sort/Sort.jsx
+++ client/src/components/Sort/Sort.jsx
@@ -1,41 +1,48 @@
 import React, { useState } from 'react'
 
-export default function Sort () {
+function Sort ({ value, onChangeSort }) {
   const [isVisible, setIsVizible] = useState(false)
-  const [selected, setSelected] = useState(0)
-  const list = ['популярности', 'цене', 'алфавиту']
-  const sortName = list[selected]
+  const list = [
+    { name: 'популярности (DESC)', sortProperty: 'rating' }, 
+    { name: 'популярности (ASC)', sortProperty: '-rating' }, 
+    { name: 'цене (DESC)', sortProperty: 'price' },
+    { name: 'цене (ASC)', sortProperty: '-price' },
+    { name: 'алфавиту (DESC)', sortProperty: 'title' },
+    { name: 'алфавиту (ASC)', sortProperty: '-title' }
+
+  ]
+
   const onClickListItem = (index) => {
-    setSelected(index)
+    onChangeSort(index)
     setIsVizible(false)
   }
 
   return (
     <div className="sort">
       <div className="sort__label">
-        <svg
+        {/* <svg
           width="10"
           height=""
           viewBox=" 0 10 6"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
           >
-          {/* <path
+          <path
             d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
             fill="#2C2C2C"
-            /> */}
-        </svg>
+            />
+        </svg> */}
             <b>Сортировка по:</b>
-        <span onClick={() => { setIsVizible(!isVisible) }}>{sortName}</span>
+        <span onClick={() => setIsVizible(!isVisible) }>{value.name}</span>
       </div>
       {isVisible && (<div className="sort__popup">
         <ul>
-          {list.map((name, index) => (
+          {list.map((obj, index) => (
             <li
               key={index}
-              onClick={() => onClickListItem(index)}
-              className={selected === index ? 'active' : ''}>
-              {name}
+              onClick={() => onClickListItem(obj)}
+              className={value.sortProperty === obj.sortProperty ? 'active' : ''}>
+              {obj.name}
             </li>
           ))}
         </ul>
@@ -43,3 +50,5 @@ export default function Sort () {
     </div>
   )
 }
+
+export default Sort
