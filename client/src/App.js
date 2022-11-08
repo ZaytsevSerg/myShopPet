f7c5bcde87e18045757ca898d423ba571bb8f917
Serg <zaytsergo@gmail.com>
2022-11-08 23:24:32 +0300

    search-all

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,13 @@ import NotFound from './pages/NotFound/NotFound'
 import Cart from './pages/Cart/Cart'
 
 function App () {
+  const [searchValue, setSearchValue] = useState('')
   return (
      <div className="wrapper">
-      <Header/>
+      <Header searchValue={searchValue} setSearchValue={setSearchValue}/>
       <div className="content">
           <Routes>
-             <Route path='/' element={<Home/>} />
+             <Route path='/' element={<Home searchValue={searchValue}/>} />
              <Route path='/cart' element={<Cart/>} />
              <Route path='*' element={<NotFound/>} />
           </Routes>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,15 +1,19 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import ReactPaginate from 'react-paginate'
 
 import Card from '../../components/Card/Card'
 import Skeleton from '../../components/Card/Skeleton'
 import Categories from '../../components/Categories/Categories'
+import Pagination from '../../components/Pagination/Pagination'
 import Sort from '../../components/Sort/Sort'
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
   const [categoryId, setCategoryId] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
+
   const [sortType, setSortType] = useState({
     name: 'популярности',
     sortProperty: 'reting'
@@ -27,14 +31,19 @@ const Home = () => {
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
     const sortBy = sortType.sortProperty.replace('-', '')
     const category = categoryId > 0 ? `category=${categoryId}` : ''
+    const search = searchValue ? `&search=${searchValue}` : ''
+
     axios.get(
-      `https://635fc15dca0fe3c21aa3b607.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}
+      `https://635fc15dca0fe3c21aa3b607.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}
         `)
       .then((res) => setItems(res.data))
       .catch((err) => console.log(err))
     setIsLoading(false)
     window.scrollTo(0, 0)
-  }, [categoryId, sortType])
+  }, [categoryId, sortType, searchValue, currentPage])
+
+  const pizzas = items.map((obj) => <Card key={obj.id}{...obj}/>)
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
 
   return (
     <div className="container">
@@ -46,11 +55,18 @@ const Home = () => {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {isLoading 
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
-              : items.map((obj) => <Card key={obj.id}{...obj}/>)
-            }
+              ? skeletons
+              : pizzas }
           </div>
+          <Pagination onChangePage={(number) => setCurrentPage(number)}/>
     </div>
   )
 }
 export default Home
+
+// const pizzas = items.filter(obj => {
+//   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+//     return true
+//   }  
+//   return false
+// }).map((obj) => <Card key={obj.id}{...obj}/>)
