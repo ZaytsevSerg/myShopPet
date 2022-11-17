701a9d538bf6fdfd1d654c18c534d3c7c1522910
Serg <zaytsergo@gmail.com>
2022-11-17 21:12:03 +0300

    fix-ok

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filter from './slice/filterSlice'
 import cart from './slice/CartSlice'
+import pizza from './slice/pizzasSlice'
 
 export const store = configureStore({
-  reducer: { filter, cart }
+  reducer: { filter, cart, pizza }
 })
